3f70c5fb1c1c8f95558bae791a7b955796abc3e5
Aashutosh <aashutoshkumar869@gmail.com>
2022-08-30 14:36:23 +0530

    first commit

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
-// index.js
-// where your node app starts
-
-// init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+	res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+	res.json({ greeting: "hello API" });
 });
 
+// endpoint for timestamp microservice
 
+app.get("/api", (req, res) => {
+	res.json({
+		unix: new Date().getTime(),
+		utc: new Date(),
+	});
+});
+
+app.get("/api/:date", (req, res) => {
+	let resJson = {};
+
+	const dateParam = isNaN(Number(req.params.date))
+		? req.params.date
+		: parseInt(req.params.date);
+
+	const date = new Date(dateParam);
+
+	isNaN(date)
+		? (resJson.error = "Invalid Date")
+		: (resJson = {
+				unix: date.getTime(),
+				utc: date.toUTCString(),
+				// much easier to produce same result as
+				// const utcDate = `${dayList[date.getDay()]}, ${date.getDate()} ${monthList[date.getMonth()]} ${date.getFullYear()} 00:00:00 GMT`;
+		  });
+
+	console.log(req.params.date, resJson);
+	return res.json(resJson);
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+const port = process.env.PORT || 8000;
+var listener = app.listen(port, function () {
+	console.log(`Your app is listening on port ${listener.address().port}`);
 });
